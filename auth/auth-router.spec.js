058205f1bd98752f2f428feefc724ba40f320427
Dominic Torruellas <dom@domtorr.com>
2019-09-16 09:22:55 -0700

    added tests for api endpoints

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,17 +1,66 @@
 const request = require('supertest');
 
-const server = require('./auth-router');
+const server = require('../api/server');
+const db = require('../database/dbConfig');
 
 describe('auth-router.js', () => {
-  describe('POST /register', () => {
-    it('should return 200 ok', async () => {
-      const res = await request(server).get('/');
-      expect(res.status).toBe(200);
-    });
+  beforeAll(async () => {
+    await db('users').truncate();
+  });
 
-    it('should return a json object', async () => {
-      const res = await request(server).get('/');
-      expect(res.type).toBe('application/json');
+  describe('POST /api/auth/register', () => {
+    it("responds with 'test' user object after creating account", done => {
+      request(server)
+        .post('/api/auth/register')
+        .send({ username: 'test', password: '1234' })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .expect(res => {
+          res.body.id = 1;
+          res.body.username = 'test';
+        })
+        .end((err, res) => (err ? done(err) : done()));
+    });
+    it("responds with 'foo' user object after creating account", done => {
+      request(server)
+        .post('/api/auth/register')
+        .send({ username: 'foo', password: 'bar' })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .expect(res => {
+          res.body.id = 2;
+          res.body.username = 'foo';
+        })
+        .end((err, res) => (err ? done(err) : done()));
+    });
+  });
+
+  describe('POST /api/auth/login', () => {
+    it(`can log in 'test' account`, done => {
+      request(server)
+        .post('/api/auth/login')
+        .send({ username: 'test', password: '1234' })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(res => {
+          expect(res.body.token).not.toBe(undefined);
+        })
+        .end((err, res) => (err ? done(err) : done()));
+    });
+    it(`gets unauthorized for incorrect password`, done => {
+      request(server)
+        .post('/api/auth/login')
+        .send({ username: 'foo', password: '1234' })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .expect(res => {
+          expect(res.body.token).toBe(undefined);
+        })
+        .end((err, res) => (err ? done(err) : done()));
     });
   });
 });
--- knexfile.js
+++ knexfile.js
@@ -8,5 +8,27 @@ module.exports = {
       tableName: 'dbmigrations',
     },
     seeds: { directory: './database/seeds' },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: { filename: './database/authTest.db3' },
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
   },
 };
